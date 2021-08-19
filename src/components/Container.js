61b23cb36352ae93c0c2ae8a5b5da92ec839e341
Jonathan Rein <jrein1296@gmail.com>
2021-08-19 17:53:52 -0400

    Removing WorkMobile component.

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Nav from './Nav';
 import About from './pages/About';
 import Work from './pages/Work';
-import WorkMobile from './pages/WorkMobile';
 import Contact from './pages/Contact';
 import Tech from './pages/Experience';
 
@@ -15,9 +14,6 @@ export default function Container() {
     }
     if (currentPage === 'Contact') {
       return <Contact />;
-    }
-    if (currentPage === 'Work' && window.innerWidth <= 879) {
-      return <WorkMobile />;
     }
     if (currentPage === 'Work') {
       return <Work />;

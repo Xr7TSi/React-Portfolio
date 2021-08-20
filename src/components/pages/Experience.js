d48cbef00fb9ae234d4589045493f818a706a1ac
Jonathan Rein <jrein1296@gmail.com>
2021-08-19 21:52:13 -0400

    Update to experience.js.

--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -28,7 +28,7 @@ function Experience() {
         <h3>Technical know-how</h3>
         <h5>Languages: JavaScript, CSS, HTML5</h5>
         <h5>Applications:  GitHub, Heroku, Firebase</h5>
-        <h5>Tools: MySQL, Express, Node.js, Bootstrap, jQuery,</h5>
+        <h5>Tools: MySQL, Express, Node.js, Bootstrap, jQuery</h5>
       </Col>
       </div>
 

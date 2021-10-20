713055a8c4f2f337ba2161ac4235c02448e68238
Jonathan Rein <jrein1296@gmail.com>
2021-10-20 15:53:48 -0400

    Updating work examples to include nginx-docker-mern-project.

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -6,14 +6,12 @@ import news from "../../assets/work-images/News-Flash-350x219.png";
 import weather from "../../assets/work-images/Weather-Dashboard-350x219.png";
 import workday from "../../assets/work-images/Work-Day-Scheduler-350x219.png";
 import amazon from "../../assets/work-images/Amazon-Clone-Home-350x219.png";
-
-
+import postCenter from "../../assets/work-images/Amazon-Clone-Home-350x219.png";
 
 const styles = {
-
   caption: {
     color: "white",
-    textAlign: "center"
+    textAlign: "center",
   },
 
   buttonPrimary: {
@@ -24,205 +22,227 @@ const styles = {
     marginRight: "5px",
     marginTop: "45px",
   },
-
-
 };
-
 
 // additional styling in styles/styles.css
 
 function Work() {
   return (
- 
-   <section>
-
-<Row>
+    <section>
+      <Row>
         <Col xs={11} md={4}>
-        <div className="image-container">
-     <img src={amazon} alt="Amazon" />
-     </div>
-      <div>
-        <h5 style={styles.caption}>
-        Amazon Clone is a full-stack e-commerce application.
-      </h5>
-      <h5 style={styles.caption}>
-        It's built with React, Firebase and Stripe.
-      </h5>
-      <div className="button-container">
-        <a
-          href="https://github.com/Xr7TSi/News-Flash"
-          className="btn btn-primary"
-          style={styles.buttonPrimary}
-          target="_blank" rel="noopener noreferrer"
-        >
-          <h5>See the repo</h5>
-        </a>
-        <a
-          href="https://clone-14498.web.app/"
-          className="btn btn-primary"
-          style={styles.buttonPrimary}
-          target="_blank" rel="noopener noreferrer"
-        >
-          <h5>See it deployed</h5>
-        </a>
-        </div>
-    </div>
+          <div className="image-container">
+            <img src={amazon} alt="Amazon" />
+          </div>
+          <div>
+            <h5 style={styles.caption}>
+              Amazon Clone is a full-stack e-commerce application.
+            </h5>
+            <h5 style={styles.caption}>
+              It's built with React, Firebase and Stripe.
+            </h5>
+            <div className="button-container">
+              <a
+                href="https://github.com/Xr7TSi/News-Flash"
+                className="btn btn-primary"
+                style={styles.buttonPrimary}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h5>See the repo</h5>
+              </a>
+              <a
+                href="https://clone-14498.web.app/"
+                className="btn btn-primary"
+                style={styles.buttonPrimary}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h5>See it deployed</h5>
+              </a>
+            </div>
+          </div>
         </Col>
 
         <Col xs={11} md={4}>
-        <div className="image-container">
-     <img src={news} alt="News Flash" />
-     </div>
-      <div>
-        <h5 style={styles.caption}>
-        News Flash gathers news based on the user's news category preferences.
-      </h5>
-      <h5 style={styles.caption}>
-        It's built with JavaScript. It uses APIs, bootstrap and local storage.
-      </h5>
-      <div className="button-container">
-        <a
-          href="https://github.com/Xr7TSi/News-Flash"
-          className="btn btn-primary"
-          style={styles.buttonPrimary}
-          target="_blank" rel="noopener noreferrer"
-        >
-          <h5>See the repo</h5>
-        </a>
-        <a
-          href="https://xr7tsi.github.io/News-Flash/"
-          className="btn btn-primary"
-          style={styles.buttonPrimary}
-          target="_blank" rel="noopener noreferrer"
-        >
-          <h5>See it deployed</h5>
-        </a>
-        </div>
-    </div>
+          <div className="image-container">
+            <img src={news} alt="News Flash" />
+          </div>
+          <div>
+            <h5 style={styles.caption}>
+              News Flash gathers news based on the user's news category
+              preferences.
+            </h5>
+            <h5 style={styles.caption}>
+              It's built with JavaScript. It uses APIs, bootstrap and local
+              storage.
+            </h5>
+            <div className="button-container">
+              <a
+                href="https://github.com/Xr7TSi/News-Flash"
+                className="btn btn-primary"
+                style={styles.buttonPrimary}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h5>See the repo</h5>
+              </a>
+              <a
+                href="https://xr7tsi.github.io/News-Flash/"
+                className="btn btn-primary"
+                style={styles.buttonPrimary}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h5>See it deployed</h5>
+              </a>
+            </div>
+          </div>
         </Col>
 
         <Col xs={11} md={4}>
-        <div className="image-container">
-     <img src={weather} alt="Weather Dashboard" />
-    </div> 
-      <div>
-        <h5 style={styles.caption}>
-        Weather Dashboard provides weather data for any city in the world.
-      </h5>
-      <h5 style={styles.caption}>
-      It's built with JavaScript. It uses APIs, bootstrap and local
-            storage.
-      </h5>
-      <div className="button-container">
-        <a
-          href="https://github.com/Xr7TSi/Weather-Dashboard"
-          className="btn btn-primary"
-          style={styles.buttonPrimary}
-          target="_blank" rel="noopener noreferrer"
-        >
-          <h5>See the repo</h5>
-        </a>
-        <a
-          href="https://xr7tsi.github.io/Weather-Dashboard/"
-          className="btn btn-primary"
-          style={styles.buttonPrimary}
-          target="_blank" rel="noopener noreferrer"
-        >
-          <h5>See it deployed</h5>
-        </a>
-        </div>
-    </div>
+          <div className="image-container">
+            <img src={weather} alt="Weather Dashboard" />
+          </div>
+          <div>
+            <h5 style={styles.caption}>
+              Weather Dashboard provides weather data for any city in the world.
+            </h5>
+            <h5 style={styles.caption}>
+              It's built with JavaScript. It uses APIs, bootstrap and local
+              storage.
+            </h5>
+            <div className="button-container">
+              <a
+                href="https://github.com/Xr7TSi/Weather-Dashboard"
+                className="btn btn-primary"
+                style={styles.buttonPrimary}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h5>See the repo</h5>
+              </a>
+              <a
+                href="https://xr7tsi.github.io/Weather-Dashboard/"
+                className="btn btn-primary"
+                style={styles.buttonPrimary}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h5>See it deployed</h5>
+              </a>
+            </div>
+          </div>
         </Col>
       </Row>
 
       <Row>
+        <Col xs={11} md={4}>
+          <div className="image-container">
+            <img src={birthday} alt="Birthday Clock" />
+          </div>
+          <div>
+            <h5 style={styles.caption}>
+              Tick Tock Birthday Clock is a place to create and share birthday
+              wishlists.
+            </h5>
+            <h5 style={styles.caption}>
+              It's built with JavaScript. It uses mysql, bcrypt, dotenv,
+              express, handlebars, bootstrap, session and sequelize.
+            </h5>
+            <div className="button-container">
+              <a
+                href="https://github.com/Xr7TSi/Tick-Tock-Birthday-Clock/"
+                className="btn btn-primary"
+                style={styles.buttonPrimary}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h5>See the repo</h5>
+              </a>
+              <a
+                href="https://tick-tock-birthday-clock.herokuapp.com/"
+                className="btn btn-primary"
+                style={styles.buttonPrimary}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h5>See it deployed</h5>
+              </a>
+            </div>
+          </div>
+        </Col>
 
-      <Col xs={11} md={4}>
-      <div className="image-container">
-     <img src={birthday} alt="Birthday Clock"/>
-    </div> 
-      <div>
-        <h5 style={styles.caption}>
-        Tick Tock Birthday Clock is a place to create and share birthday
-            wishlists.
-      </h5>
-      <h5 style={styles.caption}>
-      It's built with JavaScript. It uses mysql, bcrypt, dotenv, express,
-            handlebars, bootstrap, session and sequelize.
-      </h5>
-      <div className="button-container">
-        <a
-          href="https://github.com/Xr7TSi/Tick-Tock-Birthday-Clock/"
-          className="btn btn-primary"
-          style={styles.buttonPrimary}
-          target="_blank" rel="noopener noreferrer"
-        >
-          <h5>See the repo</h5>
-        </a>
-        <a
-          href="https://tick-tock-birthday-clock.herokuapp.com/"
-          className="btn btn-primary"
-          style={styles.buttonPrimary}
-          target="_blank" rel="noopener noreferrer"
-        >
-          <h5>See it deployed</h5>
-        </a>
-        </div>
-    </div>
-
-      </Col>
-
-      <Col xs={11} md={4}>
-      <div className="image-container">
-     <img src={workday} alt="Workday Scheduler"/>
-    </div> 
-      <div>
-        <h5 style={styles.caption}>
-        Workday Scheduler is a day planner.
-      </h5>
-      <h5 style={styles.caption}>
-      It's built with JavaScript. It uses moment.js, bootstrap and local
-            storage.
-      </h5>
-      <div className="button-container">
-        <a
-          href="https://github.com/Xr7TSi/Workday-Scheduler/"
-          className="btn btn-primary"
-          style={styles.buttonPrimary}
-          target="_blank" rel="noopener noreferrer"
-        >
-          <h5>See the repo</h5>
-        </a>
-        <a
-          href="https://xr7tsi.github.io/Workday-Scheduler/"
-          className="btn btn-primary"
-          style={styles.buttonPrimary}
-          target="_blank" rel="noopener noreferrer"
-        >
-          <h5>See it deployed</h5>
-        </a>
-        </div>
-    </div>
-</Col>
-
+        <Col xs={11} md={4}>
+          <div className="image-container">
+            <img src={workday} alt="Workday Scheduler" />
+          </div>
+          <div>
+            <h5 style={styles.caption}>Workday Scheduler is a day planner.</h5>
+            <h5 style={styles.caption}>
+              It's built with JavaScript. It uses moment.js, bootstrap and local
+              storage.
+            </h5>
+            <div className="button-container">
+              <a
+                href="https://github.com/Xr7TSi/Workday-Scheduler/"
+                className="btn btn-primary"
+                style={styles.buttonPrimary}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h5>See the repo</h5>
+              </a>
+              <a
+                href="https://xr7tsi.github.io/Workday-Scheduler/"
+                className="btn btn-primary"
+                style={styles.buttonPrimary}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h5>See it deployed</h5>
+              </a>
+            </div>
+          </div>
+        </Col>
       </Row>
 
-      
-     
-
-
-
-
-
-
-   
-
-  
-
-
-
-   </section>
- 
+      <Row>
+        <Col xs={11} md={4}>
+          <div className="image-container">
+            <img src={postCenter} alt="PostCenter" />
+          </div>
+          <div>
+            <h5 style={styles.caption}>PostCenter is a social media app.</h5>
+            <h5 style={styles.caption}>
+              It's a full-stack MERN application, deployed on an Amazon Web Services EC2 Ubuntu instance, using NGINX for reverse-proxy and Certbot for managing SSL.
+              It is fully containerized and can run in Docker containers for development. 
+              Post Center features options for Google OAuth and email login.
+            </h5>
+            <div className="button-container">
+              <a
+                href="https://github.com/Xr7TSi/Nginx-Docker-MERN-Project"
+                className="btn btn-primary"
+                style={styles.buttonPrimary}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h5>See the repo</h5>
+              </a>
+              <a
+                href="https://xr7tsi.com/"
+                className="btn btn-primary"
+                style={styles.buttonPrimary}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h5>See it deployed</h5>
+              </a>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </section>
   );
 }
 

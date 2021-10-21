c2299e2a7885e4b292366ce1638de93dbb342c91
Jonathan Rein <jrein1296@gmail.com>
2021-10-21 10:52:49 -0400

    Adding conditional rendering to haome page for better mobile experience.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,8 @@ import lava from '../../assets/animations/lava.mp4';
 
 
 function About() {
+
+  if (window.innerWidth >= 880)
   return (
   <div className="container" id="home-text">
     <video autoPlay loop muted
@@ -35,6 +37,37 @@ function About() {
       </div>
   </div>
   );
+
+  if (window.innerWidth <= 879)
+  return (
+    <div className="container" id="home-text">
+      <video autoPlay loop muted
+      style={{
+        position: 'absolute',
+        width: '95.5%',
+        left: '2.5%',
+        top: '13.5%',
+        height: '100%',
+        objectFit: 'cover',
+        transform: "translate(-50%, -50%')",
+        zIndex: '-1'
+      }}>
+        <source src={lava} type="video/mp4" />
+      </video>
+  
+        <div className="col-md-12">
+            <h1>J.D. REIN  </h1>
+            <br></br>
+            <Typist cursor={{ show: false}}>
+            <Typist.Delay ms={1500} />
+            <h3>FULL-STACK WEB DEVELOPER</h3>
+            <br></br>
+            <Typist.Delay ms={500} />
+            <h3>Working with AWS, Docker, Ubuntu, MongoDB, Express, React, Node.js and more.</h3>
+            </Typist>
+        </div>
+    </div>
+    );
 }
 
 
